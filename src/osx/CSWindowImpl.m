1274e43430be9070dff7bd3af98e61e2b6440238
Yulius Tjahjadi <yulius@omelette.local>
2012-07-30 07:29:47 -0700

    add GPL license to source files

--- src/osx/CSWindowImpl.m
+++ src/osx/CSWindowImpl.m
@@ -1,10 +1,23 @@
-//
-//  CSWindow.m
-//  CleanSlate
-//
-//  Created by Yulius Tjahjadi on 7/20/12.
-//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
-//
+/* 
+ *
+ * Copyright 2012 Yulius Tjahjadi <yultide@yahoo.com>
+ *
+ * This file is part of CleanSlate.
+ * 
+ * CleanSlate is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * CleanSlate is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License 
+ * along with CleanSlate. If not, see http://www.gnu.org/licenses/.
+ *
+ */
 
 #import "CSWindowImpl.h"
 #import "CSView.h"
