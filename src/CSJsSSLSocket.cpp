099641f007ff5eb04deb1c7c29578051b91b9f68
Yulius Tjahjadi <yulius@omelette.local>
2012-09-26 19:28:46 -0700

    implement sslsocket

---
+++ src/CSJsSSLSocket.cpp
@@ -0,0 +1,9 @@
+//
+//  CSJsSSLSocket.cpp
+//  CleanSlate
+//
+//  Created by Yulius Tjahjadi on 8/21/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#include <iostream>
---
+++ src/CSJsSSLSocket.h
@@ -0,0 +1,14 @@
+//
+//  CSJsSSLSocket.h
+//  CleanSlate
+//
+//  Created by Yulius Tjahjadi on 8/21/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#ifndef CleanSlate_CSJsSSLSocket_h
+#define CleanSlate_CSJsSSLSocket_h
+
+
+
+#endif
